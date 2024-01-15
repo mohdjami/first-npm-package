2a4d01222e3dfa2465c389e300bde0928a2a7af7
mohd jami <your_email@example.com>
2024-01-15 14:47:25 +0530

    Squashing all commits

---
+++ README.md
@@ -0,0 +1,24 @@
+# NPX CARD
+
+This my NPX card unique style to connect with me directly via console or terminal
+
+👇 just hit
+
+```bash
+npx jamii
+```
+
+And get to know me in unique style.
+
+<hr/>
+
+### 📋 About Me:
+
+I am a passionate developer from India, currently working as a Backend Web Developer. I have experience of 2 years in this Nodejs, Express, MongoDB, Mongoose.
+I am also a Fullstack Next.js Developer.
+All my projects are available on my github account: https://www.github.com/mohdjami
+
+##### STEPS TO CREATE YOUR OWN
+
+The article written by our friend @jackboberg. I used the same for the reference to deploy the package.
+[Write a Simple npx Business Card](https://studioelsa.se/blog/open-source-oss-npx-business-card).
---
+++ card.js
@@ -0,0 +1,117 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require("fs");
+const request = require("request");
+const path = require("path");
+const ora = require("ora");
+const cliSpinners = require("cli-spinners");
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+  {
+    type: "list",
+    name: "action",
+    message: "What you want to do?",
+    choices: [
+      {
+        name: `Send me an ${chalk.green.bold("email")}?`,
+        value: () => {
+          open("mailto:mohdjamikhann@gmail.com");
+          console.log("\nDone, see you soon at inbox.\n");
+        },
+      },
+      {
+        name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
+
+        value: () => {
+          // cliSpinners.dots;
+          const loader = ora({
+            text: " Downloading Resume",
+            spinner: cliSpinners.material,
+          }).start();
+
+          let pipe = request(
+            "https://student-management-system-ir8t.vercel.app//api/resume"
+          ).pipe(
+            fs.createWriteStream("./mohdjami-resume.pdf") // Change the file extension to .pdf
+          );
+
+          pipe.on("finish", function () {
+            let downloadPath = path.join(process.cwd(), "mohdjami-resume.pdf"); // Change the file extension to .pdf
+            console.log(`\nResume Downloaded at ${downloadPath} \n`);
+            open(downloadPath);
+            loader.stop();
+          });
+        },
+      },
+      {
+        name: "Just quit.",
+        value: () => {
+          console.log("Hasta la vista.\n");
+        },
+      },
+    ],
+  },
+];
+
+const data = {
+  name: chalk.bold.green("               Mohd Jami"),
+  handle: chalk.white("@mohdjami"),
+  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("mohdjami786"),
+  github: chalk.gray("https://github.com/") + chalk.green("mohdjami"),
+  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("mohdjami"),
+  web: chalk.cyan("https://mohdjami.vercel.app"),
+  npx: chalk.red("npx") + " " + chalk.white("jami"),
+
+  labelWork: chalk.red.bold("        I am a Backend Developer"),
+  labelTwitter: chalk.white.bold("    Twitter:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelWeb: chalk.white.bold("        Web:"),
+  labelCard: chalk.white.bold("       Card:"),
+};
+
+const me = boxen(
+  [
+    `${data.name}`,
+    ``,
+    `${data.labelWork}`,
+    ``,
+    `${data.labelTwitter}  ${data.twitter}`,
+    `${data.labelGitHub}  ${data.github}`,
+    `${data.labelLinkedIn}  ${data.linkedin}`,
+    `${data.labelWeb}  ${data.web}`,
+    ``,
+    `${data.labelCard}  ${data.npx}`,
+    ``,
+    `${chalk.italic("I am currently looking for new opportunities,")}`,
+    `${chalk.italic("my inbox is always open. Whether you have a")}`,
+    `${chalk.italic("question or just want to say hi, I will try ")}`,
+    `${chalk.italic("my best to get back to you!")}`,
+  ].join("\n"),
+  {
+    margin: 1,
+    float: "center",
+    padding: 1,
+    borderStyle: "single",
+    borderColor: "green",
+  }
+);
+
+console.log(me);
+const tip = [
+  `Tip: Try ${chalk.cyanBright.bold("cmd/ctrl + click")} on the links above`,
+  "",
+].join("\n");
+console.log(tip);
+
+prompt(questions).then((answer) => answer.action());
